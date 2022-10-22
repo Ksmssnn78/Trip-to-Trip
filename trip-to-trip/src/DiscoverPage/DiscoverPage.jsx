b3b0038ef7b09a1e326939c5dadf2b43d6a966aa
Mohammed-Ashrafujjaman <mashraf9881@gmail.com>
2022-10-22 16:38:46 +0600

    discover page feature added

--- trip-to-trip/src/DiscoverPage/DiscoverPage.jsx
+++ trip-to-trip/src/DiscoverPage/DiscoverPage.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DiscoverPage.css';
 import CenterModeSlick from '../CenterModeSlick/CenterModeSlick';
-
+// import CarouselItem from '../CarouselItem/CarouselItem';
+import PauseOnHover from '../PauseOnHoverSlick/PauseOnHover';
 const DiscoverPage = () => {
+    const [active, setActive] = useState(true)
+    const [imgs, setImgs] = useState([])
+    const isActive = (value,images) =>{
+            setActive(value);
+            setImgs(images);
+            console.log(imgs);
+            console.log(active);
+    }
+    
     return (
         <div id='discover_main'>
-            <CenterModeSlick></CenterModeSlick>
+            <CenterModeSlick isActive={isActive}/>
+            {/* <CarouselItem/> */}
+            <PauseOnHover active={active}/>
         </div>
     );
 };
