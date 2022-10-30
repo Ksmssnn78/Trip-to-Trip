6ed0d47ffd0c4f16dfa3eb8d23d222ad3c4c68db
Tonmoy-Roy <roytonmoy901@gmail.com>
2022-10-30 10:38:55 +0600

    AboutUs

--- trip-to-trip/src/AboutUs/AboutUs.jsx
+++ trip-to-trip/src/AboutUs/AboutUs.jsx
@@ -3,8 +3,20 @@ import './AboutUs.css';
 
 const AboutUs = () => {
     return (
-        <div>
-            
+        <div className='about'>
+            <div className='about-container'>
+            <h1 className='about-head'>About Us</h1>
+            <h5 className='about-para'><p>The main purpose of this project is to explore beautiful places on earth, make a tour around the world and learn new culture.The Website will be used to find different beautiful places all over the world, tour with us 
+                and sharing beautiful views of the world along with experiences.This platform allows users to create profiles and connect with people of their interests and thoughts to find beautiful place to make a tour.Users can also 
+                react and comment on others' posts.</p>
+                <p>Anyone can get a review on a specific Place without and difficulties.Here anyone can share their thoughts without any restrictions 
+                on Specific categories.User can Search different places around the world and also take our service for tour 
+                arrangements.</p></h5>
+
+            <footer className='footer'>
+                <small className='' > Copyright @ 2022 All right reserved by team Geekbytes</small>
+            </footer>
+             </div> 
         </div>
     );
 };
