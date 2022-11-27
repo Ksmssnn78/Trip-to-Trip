5a010c58abf38150d0ad1681b7d9e80e3c225f21
Mohammed-Ashrafujjaman <mashraf9881@gmail.com>
2022-11-27 22:31:21 +0600

    tonmoy er zamela solved

--- trip_to_trip_server/index.js
+++ trip_to_trip_server/index.js
@@ -6,11 +6,7 @@ const bodyParser = require('body-parser');
 const fs = require("fs-extra");
 const multer = require("multer");
 const path = require('path');
-<<<<<<< HEAD
 
-=======
-app.use(express.static(path.join(__dirname, '/')));
->>>>>>> ashraf
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
@@ -20,7 +16,7 @@ app.use(function(req, res, next) {
 const username = process.env.user;
 const pass = process.env.mongopass;
 const url = "mongodb+srv://"+username+":"+pass+"@cluster0.plphx1t.mongodb.net/?retryWrites=true&w=majority";
-
+app.use(express.static(path.join(__dirname, '/')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
