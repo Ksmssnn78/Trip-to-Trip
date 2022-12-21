f9ddec5c9b39043dab503782f7cd1cc0fc504a24
Mohammed-Ashrafujjaman <mashraf9881@gmail.com>
2022-12-22 01:39:40 +0600

    booking page data manupulation done

--- trip-to-trip/src/App.js
+++ trip-to-trip/src/App.js
@@ -22,18 +22,80 @@ import DetailedLocation from './DetailedLocation/DetailedLocation';
 
 function App() {
 
-  const [booking_data,setBookingData] = useState("")
+  
+  const [location_data,setLocationData] = useState("")
+  const [tempSldata, setTempSlData] = useState("");
+  const [tempSCdata, setTempSCData] = useState("");
+  const [tempEmaildata, setTempEData] = useState("");
+  const [tempUNdata, setTempUNData] = useState("");
+  const [tempFNdata, setTempFNData] = useState("");
+  const [tempLNdata, setTempLNData] = useState("");
+  const [tempADDdata, setTempADDData] = useState("");
+  const [tempCITYdata, setTempCITYData] = useState("");
+  const [spot_data, setSpotData] = useState();
+  const [userdata, setUserData] = useState();
   // console.log(booking_data);
   const [userName, setUserName] = useState("");
   
+  const getSpotDetails = async()=>{
+    const response = await fetch('http://localhost:5000/spot_info');
+    const data = await response.json();
+    setSpotData(data);
+  }
+
+  const getUserDetails = async()=>{
+    const response = await fetch('http://localhost:5000/user');
+    const data = await response.json();
+    setUserData(data);
+    console.log(data);
+  }
+
+  const filterspotdata = (lc,UE) =>{
+      if(location_data !== ""){
+        let temp = spot_data.filter(data => data.location === location_data);
+        if(temp.length !== 0)
+        {
+          setTempSlData(temp[0].location);
+          setTempSCData(temp[0].country);
+        }
+      }
+      if(UE !== ""){
+        let temp = userdata.filter(data => data.email === UE);
+        if(temp.length !== 0)
+        {
+          setTempEData(temp[0].email);
+          setTempUNData(temp[0].username);
+          setTempFNData(temp[0].fname); 
+          setTempLNData(temp[0].lname);
+          setTempADDData(temp[0].address); 
+          setTempCITYData(temp[0].city);
+        }
+
+      }
+    }
+      
+    // const filterUserdata = (UE) =>{
+      
+    // }
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       if (user) {
         setUserName(user.displayName);
       } else setUserName("");
     });
+    getSpotDetails();
+    getUserDetails();
   }, []);
 
+  useEffect(()=>{
+    filterspotdata(location_data,userName);
+  },[location_data,userName])
+
+
+  // useEffect(()=>{
+  //   filterUserdata(userName);
+  // },[userName])
 
   return (
     <div className="App">
@@ -41,16 +103,16 @@ function App() {
         <Header></Header>
         <Routes>
           <Route exact path="/Home" element={<Home />} />
-          <Route exact path="/Discover" element={<DiscoverPage set_B_DfD={setBookingData} />} />
+          <Route exact path="/Discover" element={<DiscoverPage set_B_DfD={setLocationData} />} />
           <Route exact path='/Community' element={<Community />} />
-          <Route exact path='/SpecialDeal' element={<SpecialDeal set_B_D={setBookingData}/>} />
+          <Route exact path='/SpecialDeal' element={<SpecialDeal set_B_D={setLocationData}/>} />
           <Route exact path='/AboutUs' element={<AboutUs />} />
           <Route exact path='/SignIn' element={<SignIn />} />
           <Route exact path='/Register' element={<Register />} />
           <Route exact path='/Admin' element={<Admin />} />
-          <Route exact path='/Booking' element={<Booking set_data={booking_data}/>} />
+          <Route exact path='/Booking' element={<Booking set_email={tempEmaildata} set_username={tempUNdata} set_fname={tempFNdata} set_lname={tempLNdata} set_address={tempADDdata} set_city={tempCITYdata} set_loc={tempSldata} set_cntry={tempSCdata}/>} />
           <Route exact path='/Profile' element={<Profile name={userName}/>} />
-          <Route exact path='/DetailedLocation' element={<DetailedLocation location={booking_data}/>} />
+          <Route exact path='/DetailedLocation' element={<DetailedLocation location={location_data}/>} />
           <Route exact path='/' element={<Home />} />
         </Routes>
       </Router>
--- trip-to-trip/src/BookingPage/BookingPage.jsx
+++ trip-to-trip/src/BookingPage/BookingPage.jsx
@@ -1,52 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './BookingPage.css';
 // import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import FormControlLabel from '@mui/material/FormControlLabel';
+// import FormControlLabel from '@mui/material/FormControlLabel';
 // import div from "react-bootstrap/Row";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Checkbox from '@mui/material/Checkbox';
+// import Checkbox from '@mui/material/Checkbox';
 import axios from 'axios';
+// import { wait } from '@testing-library/user-event/dist/utils';
 
 const BookingPage = (props) => {
 
+    const [errMsg, setErrMsg] = useState("");
     const [book, setBook] = useState({
         username: "",
         email: "",
         fname: "",
         lname: "",
         address: "",
-        city: ""
+        city: "",
+        hotel: "",
+        country: "",
+        location: "",
+        person: "",
+        days: "",
+        start_loc: "",
+        room: "",
+        vehicle_type: ""
     });
-    
+
+
     const submitbooking = (e) =>{
         e.preventDefault();
-        console.log(book)
-        try{
-            axios.post('http://localhost:5000/Booking/postData',book).then(respose => {
-                console.log(respose)
-            }).catch(error => {
-                console.log(error)
-            })
-            // fetch('http://localhost:5000/Booking/Data', {
-            // method: 'POST',
-            // headers: {
-            //     'Accept': 'application/json',
-            //     'Content-Type': 'application/json',
-            //     'Access-Control-Allow-Methods': '*'
-            // },
-            // body: JSON.stringify(book),
-            // })
-            // .then((response) => {
-            //     console.log("response",response)
-            // })
-            // .catch((error) => {
-            //     console.error('Error:', error);
-            // });
-
-        } catch(e){
-            console(e)
+        let check_b = document.getElementById("checkterm");
+        if(check_b.checked){
+            console.log(book);
+            console.log(props.set_loc);
+            try{
+                if (book.person === "" || book.room === "" || book.days === "" || book.start_loc === "" || book.vehicle_type === "" || book.hotel === "") {
+                    setErrMsg("Fill all fields correctly");
+                    return;
+                }else{
+                    axios.post('http://localhost:5000/Booking/postData',book).then(respose => {
+                    console.log(respose)
+                    }).catch(error => {
+                        console.log(error)
+                    })
+                }
+                
+            }catch(e){
+                console(e)
+            }
+        }else{
+            setErrMsg("please Check the Box before proceed!")
         }
         
     }   
@@ -58,57 +65,68 @@ const BookingPage = (props) => {
             <Form className="Booking-page-form" >
                     <div className='user-booking common-div-booking'>
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-username" onChange={(event) => setBook((prev) => ({ ...prev, username: event.target.value }))} defaultValue="" size="small" label="Username" variant="outlined" />
+                            <TextField id="outlined-b-username"  defaultValue="" size="small" label={props.set_username} variant="outlined" disabled={true} />
                         </Form.Group>
 
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-email" onChange={(event) => setBook((prev) => ({ ...prev, email: event.target.value }))} defaultValue="" size="small" label="Email" variant="outlined" />
+                            <TextField id="outlined-b-email"  defaultValue="" size="small" label={props.set_email} variant="outlined" disabled={true}/>
                         </Form.Group>
                     </div>
                     
 
                     <div className="name-booking common-div-booking">
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-fname" onChange={(event) => setBook((prev) => ({ ...prev, fname: event.target.value }))} defaultValue="" size="small" label="First Name" variant="outlined" />
+                            <TextField id="outlined-b-fname"  defaultValue="" size="small" label={props.set_fname} variant="outlined" disabled={true}/>
                             {/* <Form.Control className="fname" placeholder="Enter first name"></Form.Control> */}
                         </Form.Group>
 
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-lname" onChange={(event) => setBook((prev) => ({ ...prev, lname: event.target.value }))} defaultValue="" size="small" label="Last Name" variant="outlined" />
+                            <TextField id="outlined-b-lname"  defaultValue="" size="small" label={props.set_lname} variant="outlined" disabled={true} />
                         </Form.Group>
                     </div>
 
                     <div className="address-booking common-div-booking">
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-address" onChange={(event) => setBook((prev) => ({ ...prev, address: event.target.value }))} defaultValue="" size="small" label="Address" variant="outlined" />
+                            <TextField id="outlined-b-address"  defaultValue="" size="small" label={props.set_address} variant="outlined" disabled={true}/>
                         </Form.Group>
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-city" onChange={(event) => setBook((prev) => ({ ...prev, city: event.target.value }))} defaultValue="" size="small" label="City" variant="outlined" />
+                            <TextField id="outlined-b-city"  defaultValue="" size="small" label={props.set_city} variant="outlined" disabled={true} />
                         </Form.Group>
                     </div>
 
                     <div className="address-booking common-div-booking">
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-address" onChange={(event) => setBook((prev) => ({ ...prev, address: event.target.value }))} defaultValue="" size="small" label="Country" variant="outlined" />
+                            <TextField id="outlined-b-address"  defaultValue="" size="small" label={props.set_loc} variant="outlined" disabled={true}/>
                         </Form.Group>
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-city" onChange={(event) => setBook((prev) => ({ ...prev, city: event.target.value }))} defaultValue="" size="small" label="Location" variant="outlined" />
+                            <TextField id="outlined-b-city"  defaultValue="" size="small" label={props.set_cntry} variant="outlined" disabled={true}/>
                         </Form.Group>
                     </div>
 
                     <div className="address-booking common-div-booking">
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-address" onChange={(event) => setBook((prev) => ({ ...prev, address: event.target.value }))} defaultValue="" size="small" type='number' label="Person?" variant="outlined" />
+                            <TextField id="outlined-b-address" onChange={(event) => setBook((prev) => ({ ...prev, person: event.target.value }))} defaultValue="" size="small" type='number' label="Person?" variant="outlined" />
                         </Form.Group>
                         <Form.Group className="mb-booking">
-                            <TextField id="outlined-b-city" onChange={(event) => setBook((prev) => ({ ...prev, city: event.target.value }))} defaultValue="" size="small" type='number' label="Days?" variant="outlined" />
+                            <TextField id="outlined-b-city" onChange={(event) => setBook((prev) => ({ ...prev, days: event.target.value }))} defaultValue="" size="small" type='number' label="Days?" variant="outlined" />
                         </Form.Group>
                     </div>
+
+                    <div className="address-booking common-div-booking">
+                        <Form.Group className="mb-booking">
+                            <TextField id="outlined-b-address" onChange={(event) => setBook((prev) => ({ ...prev, room: event.target.value }))} defaultValue="" size="small" type='number' label="room?" variant="outlined" />
+                        </Form.Group>
+                        <Form.Group className="mb-booking">
+                            <TextField id="outlined-b-city" onChange={(event) => setBook((prev) => ({ ...prev, start_loc: event.target.value }))} defaultValue="" size="small" label="Start location(city)" variant="outlined" />
+                        </Form.Group>
+                    </div>
+
 
                     <div className="address-booking common-div-booking">
                         <div className="mb-booking">
-                            <label>Choose a Hotel: </label>
-                            <select  name="cars" id="cars">
+                            <label className='mb-booking-select'>Choose a Hotel : </label>
+                            <select  name="Hotel" onChange={(event) => setBook((prev) => ({ ...prev, hotel: event.target.value }))}>
+                                <option value=""></option>
                                 <option value="volvo">Volvo</option>
                                 <option value="saab">Saab</option>
                                 <option value="opel">Opel</option>
@@ -116,25 +134,24 @@ const BookingPage = (props) => {
                             </select>
                         </div>
                         <div className="mb-booking">
-                            <label>Choose a Flight: </label>
-                            <select  name="cars" id="cars">
-                                <option value="volvo">Volvo</option>
-                                <option value="saab">Saab</option>
-                                <option value="opel">Opel</option>
-                                <option value="audi">Audi</option>
+                            <label className='mb-booking-select'>Choose a Vehicle type : </label>
+                            <select  name="vehicle_type" onChange={(event) => setBook((prev) => ({ ...prev, vehicle_type: event.target.value }))}>
+                                <option value=""></option>
+                                <option value="BUS">BUS</option>
+                                <option value="Ship">Ship</option>
+                                <option value="Plane">plane</option>
                             </select>
                         </div>     
                     </div>
 
-                    <Form.Group className="mb-booking">
-                        <FormControlLabel control={<Checkbox />} label="Yes, I agree with all terms & conditions" />
-                    </Form.Group>
+                    <input type="checkbox" className='mb-booking' id='checkterm' name='checkbox' />
+                    <label id='checkbox_label' htmlFor="checkbox">Yes, I agree with all terms & conditions</label>
 
                     <Form.Group id="btn-booking">
-                        <Button id="btn-b-submit" onClick={submitbooking} variant="contained">Submit</Button>
+                        <Button id="btn-b-submit" onClick={submitbooking} variant="contained">Proceed</Button>
                     </Form.Group>
 
-                    
+                    <b id="mb-error">{errMsg}</b>
                 </Form >
         </div>
     );
